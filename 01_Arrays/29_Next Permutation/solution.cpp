ab40456bc44e63636f9fabf073b10e4b4978b273
SV Sai Satya <102280565+satya5555@users.noreply.github.com>
2026-03-11 21:07:28 +0530

    Implement next permutation algorithm in C++

--- 01_Arrays/29_Next Permutation/solution.cpp
+++ 01_Arrays/29_Next Permutation/solution.cpp
@@ -1,1 +1,22 @@
-
+#include <iostream>
+#include <algorithm> 
+class Solution {
+public:
+    void nextPermutation(vector<int>& nums) {
+        int i =nums.size()-2;
+        while(i>=0 && nums[i+1]<=nums[i])
+        {
+            i-=1;
+        }
+        if (i>=0)
+        {
+            int j = nums.size()-1;
+            while(nums[j]<=nums[i])
+            {
+                j-=1;
+            }
+            swap(nums[i],nums[j]);
+        }
+        reverse(nums.begin()+i+1,nums.end());
+    }
+};
